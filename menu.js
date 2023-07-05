e24458181f6d46872d5ca2900c1cc5e46a8c02a1
Nuket <120319808+nukettas@users.noreply.github.com>
2023-07-05 00:24:01 -0400

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,32 @@
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
+
+const mainMenu = document.querySelector(".container");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const menu_items = document.querySelectorAll("nav .container li a");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+  });
+});
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+function close() {
+  mainMenu.style.top = "-100%";
+}
